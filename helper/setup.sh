32fc503e3dee9c8f9ec847f2dc7e793e909ddd74
Calvin Cheung <calvincheung0630@gmail.com>
2021-06-04 15:06:10 +0800

    feat: Centralised dep

--- helper/setup.sh
+++ helper/setup.sh
@@ -12,6 +12,4 @@ git config --global user.email 419955347@qq.com
 git config --global user.name mingming741
 python3 get-pip.py
 python3 -m pip install --upgrade pip
-python3 -m pip install matplotlib
-python3 -m pip install pandas
-python3 -m pip install requests
+python3 -m pip -r requirements.txt
